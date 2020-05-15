e070888bdf2c5776cdd7f31feab33377b0e45ebc
Naman Arora <arora.naman@protonmail.com>
2020-05-15 19:20:07 -0400

    Added no fork ck variable to enable debugging

--- src/tests/main_test.c
+++ src/tests/main_test.c
@@ -6,6 +6,7 @@ int main(int argc, char **argv)
 {
 	Suite *rec_s=rec_suite();
 	SRunner *rec_sr=srunner_create(rec_s);
+	srunner_set_fork_status(rec_sr, CK_NOFORK);
 
 	srunner_run_all(rec_sr, CK_NORMAL);
 	srunner_free(rec_sr);
