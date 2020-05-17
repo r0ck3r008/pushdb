e181c8172d5b952d98a41cfe8deb03347a97782e
Naman Arora <arora.naman@protonmail.com>
2020-05-17 01:26:55 -0400

    added runner for parser module

--- src/tests/main_test.c
+++ src/tests/main_test.c
@@ -1,6 +1,7 @@
 #include<check.h>
 
 #include"fs/tests.h"
+#include"parser/tests.h"
 
 int main(int argc, char **argv)
 {
@@ -10,6 +11,12 @@ int main(int argc, char **argv)
 	srunner_set_fork_status(fs_sr, CK_NOFORK);
 	srunner_run_all(fs_sr, CK_NORMAL);
 
+	//parser
+	Suite *parse_s=parser_suite();
+	SRunner *parser_sr=srunner_create(parse_s);
+	srunner_set_fork_status(parser_sr, CK_NOFORK);
+	srunner_run_all(parser_sr, CK_NORMAL);
 
 	srunner_free(fs_sr);
+	srunner_free(parser_sr);
 }
