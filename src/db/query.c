a407aa6c35f17c8bb0520c0edf8bfa8497af952f
Naman Arora <arora.naman@protonmail.com>
2020-05-17 01:21:57 -0400

    Added a query structure to gracefully handle parse output structures

---
+++ src/db/query.c
@@ -0,0 +1,32 @@
+#define QUERY_INTERNAL
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+
+#include"alloc.h"
+#include"query.h"
+
+Query *query_init(const char *str)
+{
+	if(str==NULL && yyparse()) {
+		fprintf(stderr, "[-]Query: Error in query parsing!\n");
+		_exit(-1);
+	} else {
+		YY_BUFFER_STATE buffer=yy_scan_string(str);
+		yyparse();
+		yy_delete_buffer(buffer);
+	}
+
+	Query *q=db_query_alloc();
+	q->tbl=tables;
+	q->alist=boolean;
+	q->sel_atts=attsToSelect;
+	q->qtype=queryType;
+	q->opvar=outputVar;
+	q->tbl_name=tableName;
+	q->ins_fname=fileToInsert;
+	q->cr_atts=attsToCreate;
+
+	return q;
+}
---
+++ src/db/query.h
@@ -0,0 +1,43 @@
+#ifndef QUERY_H
+#define QUERY_H
+
+#include<stdint.h>
+
+#include"parser/parse_tree.h"
+
+#ifdef QUERY_INTERNAL
+
+extern TableList *tables;
+extern AndList *boolean;
+extern NameList *attsToSelect;
+extern int queryType;
+extern char *outputVar;
+extern char *tableName;
+extern char *fileToInsert;
+extern AttrList *attsToCreate;
+
+typedef struct yy_buffer_state * YY_BUFFER_STATE;
+extern int yyparse(void);
+extern YY_BUFFER_STATE yy_scan_string();
+extern void yy_delete_buffer(YY_BUFFER_STATE);
+
+//extern int yy_parse(void);
+//extern YY_BUFFER_STATE yy_scan_string(const char *);
+//extern void yy_delete_buffer();
+
+#endif
+
+typedef struct Query
+{
+	TableList *tbl;
+	AndList *alist;
+	NameList *sel_atts;
+	uint8_t qtype;
+	int8_t *opvar, *tbl_name, *ins_fname;
+	AttrList *cr_atts;
+} Query;
+
+Query *query_init(const char *);
+//void query_deinit(Query *);
+
+#endif
