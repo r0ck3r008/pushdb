6020f6d0f253c053139a583d1b9d225d99c2ddc4
Naman Arora <arora.naman@protonmail.com>
2020-05-19 14:58:39 -0400

    unique attribute names

--- src/tests/fs/record_test.c
+++ src/tests/fs/record_test.c
@@ -10,15 +10,13 @@ Schema *prep_schema()
 {
 	int8_t *sch_name=strdup("test_name");
 	int8_t *fname=strdup("dummy_fname");
-	int8_t *att_name=strdup("att_name");
 	Schema *sch=schema_init(sch_name, fname);
-	schema_add_att(sch, att_name, 0, 0);
-	schema_add_att(sch, att_name, 0, 1);
-	schema_add_att(sch, att_name, 10, 2);
+	schema_add_att(sch, "att1", 0, 0);
+	schema_add_att(sch, "att2", 0, 1);
+	schema_add_att(sch, "att3", 10, 2);
 
 	free(sch_name);
 	free(fname);
-	free(att_name);
 
 	return sch;
 }
