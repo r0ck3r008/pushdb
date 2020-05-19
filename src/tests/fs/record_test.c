16b8d99cd8d5a63ee08ace9259712421fa207a9d
Naman Arora <arora.naman@protonmail.com>
2020-05-19 14:59:30 -0400

    updated definition of record_deser, refer commit 553c1b34b

--- src/tests/fs/record_test.c
+++ src/tests/fs/record_test.c
@@ -28,7 +28,7 @@ START_TEST(SERIALIZATION_TEST)
 	int8_t *delimiter=strdup("|");
 	int8_t *rec_str=strdup("1|1.010|Hello");
 	Record *rec=record_ser(rec_str, delimiter, sch);
-	int8_t *ret=record_deser(rec, delimiter, sch);
+	int8_t *ret=record_deser(rec, sch, delimiter);
 
 	//test
 	ck_assert_str_eq(rec_str, ret);
