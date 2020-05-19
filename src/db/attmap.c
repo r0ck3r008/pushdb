c488b85866f522fa70f70382cf2466deddcba585
Naman Arora <arora.naman@protonmail.com>
2020-05-19 01:59:54 -0400

    redesigned attribute map as linked attribute map, which supports sequential slower sequential access too

--- src/db/attmap.c
+++ src/db/attmap.c
@@ -5,62 +5,84 @@
 #include"alloc.h"
 #include"attmap.h"
 
-uint8_t attmap_hash_it(int8_t *name)
+uint8_t hash_it(int8_t *key)
 {
 	uint8_t indx=0;
-	while(*name!='\0') {
-		indx+=*name;
-		name+=1;
+	while(*key!='\0') {
+		indx+=*key;
+		key+=1;
 	}
 
 	return indx%ATTMAP_SIZE;
 }
 
-Attribute **attmap_init()
+AttMap *attmap_init()
 {
-	Attribute **map=db_attributep_alloc(ATTMAP_SIZE);
+	AttMap *map=db_attmap_alloc();
+
 	return map;
 }
 
-void attmap_add_att(Attribute **map, int8_t *name, DataType type,
-			uint16_t len)
-{
-	uint8_t indx=attmap_hash_it(name);
-	Attribute *attr=db_attribute_alloc();
-	attr->name=db_char_copy(name);
-	attr->type=type;
-	attr->len=len;
-	if(map[indx]==NULL) {
-		map[indx]=attr;
-	} else {
-		Attribute *curr=map[indx];
-		while(!(curr->next!=NULL && (curr=curr->next)));
-		curr->next=attr;
-	}
-}
-
-Attribute *attmap_find_att(Attribute **map, int8_t *name)
-{
-	uint8_t indx=attmap_hash_it(name);
-	Attribute *curr=map[indx];
-	if(curr!=NULL && curr->next!=NULL)
-		while(!(curr->next!=NULL && (curr=curr->next)));
-
-	return curr;
-}
-
-void attmap_deinit(Attribute **map)
+void attmap_deinit(AttMap *map)
 {
 	for(int i=0; i<ATTMAP_SIZE; i++) {
-		if(map[i]->next!=NULL) {
-			Attribute *curr=map[i]->next;
-			while(map[i]->next!=NULL) {
-				map[i]->next=curr->next;
+		if(map->map[i]!=NULL) {
+			Attribute *curr=map->map[i]->nxt;
+			while(map->map[i]->nxt!=NULL) {
+				map->map[i]->nxt=curr->nxt;
 				free(curr);
 			}
+			free(map->map[i]);
 		}
-		free(map[i]);
+	}
+	free(map->map);
+	free(map);
+}
+
+void attmap_add_att(AttMap *map, int8_t *aname, DataType type, uint16_t len)
+{
+	//create attribute
+	uint16_t indx=hash_it(aname);
+	Attribute *att=db_attribute_alloc();
+	att->name=db_char_copy(aname);
+	att->type=type;
+	att->pos=map->tot_len;
+
+	//update map
+	if(map->head==NULL)
+		map->head=att;
+	else
+		map->tail->nxt_sq=att;
+	map->tail=att;
+	if(map->map[indx]==NULL) {
+		map->map[indx]=att;
+	} else {
+		Attribute *curr=map->map[indx];
+		while(!(curr->nxt!=NULL && (curr=curr->nxt)));
+		curr->nxt=att;
+	}
+	if(type==Int)
+		map->tot_len+=sizeof(uint16_t);
+	else if(type==Float)
+		map->tot_len+=sizeof(float);
+	else if(type==String)
+		map->tot_len+=len;
+}
+
+Attribute *attmap_find(AttMap *map, int8_t *name, uint16_t *len)
+{
+	uint16_t indx=hash_it(name);
+	Attribute *curr=map->map[indx];
+	if(curr!=NULL && curr->nxt!=NULL)
+		while(!(curr!=NULL && strcmp(name, curr->name) &&
+			(curr=curr->nxt)));
+
+	if(curr!=NULL && curr->type==String) {
+		if(map->tail!=curr)
+			*len=((curr->nxt_sq->pos)-(curr->pos));
+		else
+			*len=((map->tot_len)-(curr->pos));
 	}
 
-	free(map);
+	return curr;
 }
--- src/db/attmap.h
+++ src/db/attmap.h
@@ -1,24 +1,31 @@
 #ifndef ATTMAP_H
 #define ATTMAP_H
+#define ATTMAP_SIZE 64
 
 #include<stdint.h>
 
 #include"glbl/defs.h"
 
-#define ATTMAP_SIZE 16
-
 typedef struct Attribute
 {
 	int8_t *name;
 	DataType type;
-	uint16_t len;
-	struct Attribute *next;
+	uint16_t pos;
+	struct Attribute *nxt, *nxt_sq;
 } Attribute;
 
-Attribute **attmap_init();
-void attmap_deinit(Attribute **);
-void attmap_add_att(Attribute **, int8_t *,
-			DataType, uint16_t);
-Attribute *attmap_find(Attribute **, int8_t *);
+typedef struct AttMap
+{
+	Attribute **map;
+	Attribute *head, *tail;
+	uint16_t tot_len;
+} AttMap;
+
+AttMap *attmap_init();
+void attmap_deinit(AttMap *);
+void attmap_add_att(AttMap *, int8_t *, DataType,
+			uint16_t);
+Attribute *attmap_find(AttMap *, int8_t *,
+			uint16_t *);
 
 #endif
