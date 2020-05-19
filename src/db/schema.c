36b65398f4fdaf76d0588e9344b5c915dc15e500
Naman Arora <arora.naman@protonmail.com>
2020-05-19 02:00:55 -0400

    Redundant attribute in schema, natts

--- src/db/schema.c
+++ src/db/schema.c
@@ -11,21 +11,17 @@ Schema *schema_init(int8_t *name, int8_t *fname)
 	sch->name=db_char_copy(name);
 	sch->fname=db_char_copy(fname);
 	sch->map=attmap_init();
-	sch->natts=0;
 
 	return sch;
 }
 
 void schema_add_att(Schema *sch, int8_t *aname, uint16_t len, DataType type)
 {
-	uint8_t n=sch->natts;
 	attmap_add_att(sch->map, aname, type, len);
-	sch->natts++;
 }
 
 void schema_deinit(Schema *sch)
 {
-	uint8_t n=sch->natts;
 	attmap_deinit(sch->map);
 	free(sch->name);
 	free(sch->fname);
