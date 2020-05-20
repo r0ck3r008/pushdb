a882e10fb40c2818d9c6aff0f089d84acb5351a2
Naman Arora <arora.naman@protonmail.com>
2020-05-20 15:16:15 -0400

    Updated skeleton operation structures

--- src/relop/selop.c
+++ src/relop/selop.c
@@ -1,1 +1,13 @@
+#include<stdio.h>
+#include<pthread.h>
+
+#include"comparator/compare.h"
 #include"selop.h"
+
+void *selop_run(void *arg)
+{
+	SelOp *args=(SelOp *)arg;
+	Cnf *cnf=cnf_init_ll(args->sch, args->alist);
+
+	pthread_exit(NULL);
+}
--- src/relop/selop.h
+++ src/relop/selop.h
@@ -1,6 +1,19 @@
 #ifndef SELOP_H
 #define SELOP_H
 
+#include<stdint.h>
 
+#include"db/schema.h"
+#include"parser/parse_tree.h"
+#include"fs/file.h"
+
+typedef struct
+{
+	Schema *sch;
+	File *file;
+	AndList *alist;
+} SelOp;
+
+void *selop_run(void *);
 
 #endif
