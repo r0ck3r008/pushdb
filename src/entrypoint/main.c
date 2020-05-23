a59c86718f82facee384786c5d915efece9945e1
Naman Arora <arora.naman@protonmail.com>
2020-05-22 22:08:15 -0400

    added a mandatory switch to input the file from where to expect query as cmdline arg

--- src/entrypoint/main.c
+++ src/entrypoint/main.c
@@ -1,9 +1,26 @@
+#include<ctype.h>
 #include<stdio.h>
 #include<stdint.h>
+#include<stdlib.h>
+#include<string.h>
+#include<unistd.h>
 
 #include"db/query.h"
 
 uint8_t main(uint8_t argc, int8_t **argv)
 {
-	Query *q=query_init(NULL);
+	if(argc!=2) {
+		fprintf(stderr,
+			"[-] Usage: bin/pushdb.out <ip_fname>{- or path}\n");
+		_exit(-1);
+	}
+	FILE *f=NULL;
+	if(!strcmp(argv[1], "-"))
+		f=stdin;
+	else
+		f=fopen(argv[1], "r");
+	if(f==NULL) {
+		fprintf(stderr, "[-]Error in opening the file %s\n", argv[1]);
+		_exit(-1);
+	}
 }
