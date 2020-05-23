3644e0abb1f89be1a256791f984e42fc16c6bff7
Naman Arora <arora.naman@protonmail.com>
2020-05-23 03:11:57 -0400

    added an internal function to open files safely

--- src/db/schema.c
+++ src/db/schema.c
@@ -1,6 +1,10 @@
 #include<stdio.h>
 #include<string.h>
 #include<stdlib.h>
+#include<sys/types.h>
+#include<sys/stat.h>
+#include<unistd.h>
+#include<errno.h>
 
 #include"alloc.h"
 #include"schema.h"
@@ -20,6 +24,27 @@ void schema_add_att(Schema *sch, int8_t *aname, uint32_t len, DataType type)
 	attmap_add_att(sch->map, aname, type, len);
 }
 
+FILE *fhandle(int8_t *fname, int8_t *perm)
+{
+	FILE *f=NULL;
+	struct stat buf;
+	uint8_t ret=stat(fname, &buf);
+	if(!strcmp(perm, "w") && !ret) {
+		fprintf(stderr, "[-]Schema: Overwriting existing schema!\n");
+		if(unlink(fname)<0) {
+			fprintf(stderr, "[-]Unlink: Error!\n");
+			_exit(-1);
+		}
+	} else if(!strcmp(perm, "r") && ret==-1) {
+		fprintf(stderr, "[-]Schema: No such schema exists!\n");
+		return NULL;
+	}
+
+	if((f=fopen(fname, perm))==NULL)
+		fprintf(stderr, "[-]Schema: Error in opening sch file!\n");
+
+	return f;
+}
 void schema_deinit(Schema *sch)
 {
 	attmap_deinit(sch->map);
