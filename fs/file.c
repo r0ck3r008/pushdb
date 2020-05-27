d599d771f7b41c2d313e27a4e5781c63ecaab6d2
Naman Arora <arora.naman@protonmail.com>
2020-05-27 01:00:42 -0400

    indent fix

--- fs/file.c
+++ fs/file.c
@@ -10,126 +10,121 @@
 
 void addFirst (Record *t, Page *target)
 {
-    if(target->head == NULL){
-        target->head = (struct Node*) malloc(sizeof(struct Node));
-        target->last = (struct Node*) malloc(sizeof(struct Node));
-        target->last = target->head;
-        target->head->val = t;
-        target->head->next = NULL;
-        return;
-    }
-    Node* temp = (struct Node*) malloc(sizeof(struct Node));
-    temp->val = t;
-    temp->next = target->head;
-    target->head = temp;
+	if(target->head == NULL){
+		target->head = (struct Node*) malloc(sizeof(struct Node));
+		target->last = (struct Node*) malloc(sizeof(struct Node));
+		target->last = target->head;
+		target->head->val = t;
+		target->head->next = NULL;
+		return;
+	}
+	Node* temp = (struct Node*) malloc(sizeof(struct Node));
+	temp->val = t;
+	temp->next = target->head;
+	target->head = temp;
 }
 
 void addLast (Record *t, Page *target)
 {
-    if(target->head == NULL){
-        target->head = (struct Node*) malloc(sizeof(struct Node));
-        target->last = (struct Node*) malloc(sizeof(struct Node));
-        target->last = target->head;
-        target->head->val = t;
-        target->head->next = NULL;
-        return;
-    }
-    Node *temp = (struct Node*) malloc(sizeof(struct Node));
-    temp->val = t;
-    temp->next = NULL;
-    target->last->next = temp;
-    target->last = temp;
+	if(target->head == NULL){
+		target->head = (struct Node*) malloc(sizeof(struct Node));
+		target->last = (struct Node*) malloc(sizeof(struct Node));
+		target->last = target->head;
+		target->head->val = t;
+		target->head->next = NULL;
+		return;
+	}
+	Node *temp = (struct Node*) malloc(sizeof(struct Node));
+	temp->val = t;
+	temp->next = NULL;
+	target->last->next = temp;
+	target->last = temp;
 }
 
 //Page
 Page *createPage()
 {
-    Page *pg = malloc(sizeof(Page));
-    return pg;
+	Page *pg = malloc(sizeof(Page));
+	return pg;
 }
 
 void EmptyItOut (Schema *target,Page *rec)
 {
-    // get rid of all of the records
-    while (1) {
-        Record temp;
-        if (!GetFirst (&temp,target,rec))
-            break;
-    }
+	// get rid of all of the records
+	while (1) {
+		Record temp;
+		if (!GetFirst (&temp,target,rec))
+			break;
+	}
 
-    // reset the page size
-    rec->curSizeInBytes = sizeof (int);
-    rec->numRecs = 0;
+	// reset the page size
+	rec->curSizeInBytes = sizeof (int);
+	rec->numRecs = 0;
 }
-
-// NOTE
-// In the append function, since the page has already been allocated and accessed,
-// There is no need for page == NULL check in addLast
-// And since the page is and internal DS to File abstraction, File can manage the allocation for the page
 
 int Append (Record *rec, Schema *sch, Page *pg)
 {
-    uint32_t len = sch->map->tot_len;
+	uint32_t len = sch->map->tot_len;
 
-    //check if the record can fit inside the page
-    if((pg->curSizeInBytes + len ) > PAGE_SIZE)
-        return 0;
+	//check if the record can fit inside the page
+	if((pg->curSizeInBytes + len ) > PAGE_SIZE)
+		return 0;
 
-    addLast(rec, pg);
-    pg->numRecs++;
-    return 1;
+	addLast(rec, pg);
+	pg->numRecs++;
+	return 1;
 }
 
 int GetFirst (Record *firstOne,Schema *target, Page *rec)
 {
-    if(rec->head == NULL){
-        return 0;
-    }
-    Node *temp = rec->head;
-    rec->head = rec->head->next;
-    free(temp);
-    int8_t *b = firstOne->bits;
-    rec->curSizeInBytes -= target->map->tot_len;
-    return 1;
+	if(rec->head == NULL){
+		return 0;
+	}
+	Node *temp = rec->head;
+	rec->head = rec->head->next;
+	free(temp);
+	int8_t *b = firstOne->bits;
+	rec->curSizeInBytes -= target->map->tot_len;
+	return 1;
 }
 
 void ToBinary (int8_t *bits, Schema *target, Page *rec)
 {
-    bits[0] = rec->numRecs;
-    int8_t *curPos = bits + sizeof(int);
+	bits[0] = rec->numRecs;
+	int8_t *curPos = bits + sizeof(int);
 
-    //copy the records
-    int i; 
-    Node *temp = rec->head;
-    for(i = 0;i < rec->numRecs; i++) {
-        int8_t *b = temp->val->bits;
-        memcpy (curPos, b, target->map->tot_len);
-        curPos += target->map->tot_len;
-        temp = temp->next;
-    }  
+	//copy the records
+	int i;
+	Node *temp = rec->head;
+	for(i = 0;i < rec->numRecs; i++) {
+		int8_t *b = temp->val->bits;
+		memcpy (curPos, b, target->map->tot_len);
+		curPos += target->map->tot_len;
+		temp = temp->next;
+	}
 
 }
 
 void FromBinary (int8_t *bits, Schema *target, Page *rec)
 {
-    rec->numRecs = target->map->tot_len;
-    int8_t* curPos = bits + sizeof (int);
-    Node *temp = rec->head;
-    while (temp!=NULL) {
-        Node *temp1 = temp;
-        free(temp);
-        temp = temp->next;
-    }
-    Node *temp1;
-    rec->curSizeInBytes = sizeof (int);
-    int i;
-    for(i=0;i<rec->numRecs;i++) {
-        int len = curPos[0];
-        rec->curSizeInBytes += len;
-        memcpy (temp1->val->bits, bits, len);
-        addLast(temp1,rec);
-    }
-    free(temp1);
+	rec->numRecs = target->map->tot_len;
+	int8_t* curPos = bits + sizeof (int);
+	Node *temp = rec->head;
+	while (temp!=NULL) {
+		Node *temp1 = temp;
+		free(temp);
+		temp = temp->next;
+	}
+	Node *temp1;
+	rec->curSizeInBytes = sizeof (int);
+	int i;
+	for(i=0;i<rec->numRecs;i++) {
+		int len = curPos[0];
+		rec->curSizeInBytes += len;
+		memcpy (temp1->val->bits, bits, len);
+		addLast(temp1,rec);
+	}
+	free(temp1);
 }
 
 // File
