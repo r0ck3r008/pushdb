c5bd18b50a11b2bd3742a35389bee5bb062de5b8
Naman Arora <arora.naman@protonmail.com>
2020-06-13 19:01:46 -0400

    refer commit 845c0c6d3

--- handler/ddl.c
+++ handler/ddl.c
@@ -45,7 +45,11 @@ int ddl_insert(Query *q)
 	sch->delim=db_char_copy(q->delim);
 	char fname[128];
 	sprintf(fname, "tmp/%s.bin", q->tbl_name);
-	File *fbin=file_load(fname, sch);
+	if(!file_load(q->ins_fname, fname, sch)) {
+		logger_msg(logger, LOG_ERR,
+			"[-]DDL: Error in loading file %s\n", q->ins_fname);
+		return 0;
+	}
 
 	file_close(fbin);
 	schema_write(sch);
