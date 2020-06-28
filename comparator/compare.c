292adb6f0766a1fb1ba2543c74e1ba666feb355f
Naman Arora <arora.naman@pm.me>
2020-06-28 18:11:23 -0400

    Added template compare family functions

---
+++ comparator/compare.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+
+#include"compare.h"
+#include"clogger/clogger.h"
+
+int compare(Record *rec, Cnf *cnf)
+{
+
+}
---
+++ include/compare.h
@@ -0,0 +1,13 @@
+#ifndef COMPARE_H
+#define COMPARE_H
+
+#include"record.h"
+#include"cnf.h"
+
+// Returns -1, 1 if rejected or accepted respectively
+int compare(Record *, Cnf *);
+// Returns -1, 0, 1 if left is less than, equal to or greater than right,
+// respectively
+int compare(Record *, Record *, Cnf *);
+
+#endif
