78df244b6d683367e712403cfe93fad0d3922a9c
Naman Arora <arora.naman@pm.me>
2020-07-01 20:47:51 -0400

    A funtion that gets total pages in file.
    	This function can help remove the need to add an index page to
    each file just to store the number of pages. It uses fstat function to
    fetch the size of the file and finds the number of pages by dividing by
    PAGE_SIZE.

--- fs/file.c
+++ fs/file.c
@@ -3,11 +3,28 @@
 #include<string.h>
 #include<sys/types.h>
 #include<sys/stat.h>
+#include<fcntl.h>
 #include<unistd.h>
 #include<errno.h>
 
 #include"clogger/clogger.h"
+#include"alloc.h"
+#include"defs.h"
 #include"file.h"
 
 extern Logger *logger;
 extern char *tmp_prefix;
+
+int file_getnpgs(int fd)
+{
+	struct stat buf;
+	explicit_bzero(&buf, sizeof(struct stat));
+	if(fstat(fd, &buf)<0) {
+		logger_msg(logger, LOG_ERR,
+			"FILE: Fstat: %s", strerror(errno));
+		return -1;
+	}
+
+	return ((int)buf.st_size/PAGE_SIZE);
+}
+
