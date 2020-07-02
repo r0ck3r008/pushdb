5b1306a5b6afc07902f2e203d50f5657262dff18
Naman Arora <arora.naman@pm.me>
2020-07-01 20:50:42 -0400

    Reimplemented the create function for file.
    	This implementation basically reads in N_CACHE_PAGE pages when
    the opened file has already been written to have a cache up and ready
    when file is opened.

--- fs/file.c
+++ fs/file.c
@@ -43,3 +43,36 @@ void file_add_pg(File *fbin, Page *pg)
 	fbin->npgs++;
 }
 
+File *file_create(char *fbin_name, Schema *sch, int flag)
+{
+	int mode=O_RDWR;
+	if(!flag)
+		mode|=O_CREAT;
+
+	File *fbin=fs_file_alloc();
+	if((fbin->fd=open(fbin_name, mode, 0644))<0) {
+		logger_msg(logger, LOG_ERR,
+			"FILE: Open: %s: %s", fbin_name, strerror(errno));
+		return NULL;
+	}
+
+	if(flag) {
+		fbin->tot_pgs=file_getnpgs(fbin->fd);
+		int npgs=(fbin->npgs < N_CACHE_PAGES) ?
+			(fbin->npgs) : (N_CACHE_PAGES);
+		for(int i=0; i<npgs; i++) {
+			char buf[PAGE_SIZE];
+			if(read(fbin->fd, buf, PAGE_SIZE*sizeof(char))<0) {
+				logger_msg(logger, LOG_ERR,
+				"FILE: Read: %s: %s", fbin_name, strerror(errno));
+				return NULL;
+			}
+			Page *pg=page_frombin(buf, sch);
+			file_add_pg(fbin, pg);
+		}
+		if(fbin->tot_pgs==npgs)
+			fbin->lst_pg=fbin->pg_tail;
+	}
+
+	return fbin;
+}
