eea65ac6f00dd90ccdcbb5799a049afb31637667
Naman Arora <arora.naman@pm.me>
2020-07-03 15:26:57 -0400

    file_add_pg is a candidate for future implementation.

--- fs/file.c
+++ fs/file.c
@@ -28,21 +28,6 @@ int file_getnpgs(int fd)
 	return ((int)buf.st_size/PAGE_SIZE);
 }
 
-void file_add_pg(File *fbin, Page *pg)
-{
-	if(fbin->npgs==N_CACHE_PAGES) {
-		logger_msg(logger, LOG_WRN,
-			"FILE: PageAdd: Deleting cached page!");
-	}
-
-	if(fbin->pg_head==NULL)
-		fbin->pg_head=pg;
-	else
-		fbin->pg_tail->next=pg;
-	fbin->pg_tail=pg;
-	fbin->npgs++;
-}
-
 File *file_create(char *fbin_name, Schema *sch, int flag)
 {
 	int mode=O_RDWR;
