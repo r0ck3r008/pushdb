0babd5e0c8e536b4cb47efe4d3d79bc694fbd89b
Naman Arora <arora.naman@pm.me>
2020-07-06 01:48:31 -0400

    File cache is now fcache module.
    	File cache module is renamed to more suitable and short, fcache.
    	Implemented functions definitions that the file module will use
    	to communicate to fcache. This module can be further enhanced to
    	encompass more complex page cache algorithms.

---
+++ include/fcache.h
@@ -0,0 +1,25 @@
+#ifndef FCACHE_H
+#define FCACHE_H
+
+#include"page.h"
+#include"file.h"
+
+typedef struct
+{
+	// Total synced pages
+	int tot_pgs;
+	// Total pages, cached + uncached
+	int npgs;
+	// Total cached pages
+	int cache_pgs;
+	// Page cache list
+	Page *pg_head, *pg_tail;
+} FCache;
+
+int fcache_getnpgs(FCache *, int);
+int fcache_addpg(FCache *, Page *, int);
+Page *fcache_getpg(FCache *, int, int);
+int fcache_syncpg(FCache *, Page *);
+void fcache_deinit(FCache *);
+
+#endif
--- include/file_cache.h
+++
@@ -1,7 +0,0 @@
-#ifndef FILE_CACHE_H
-#define FILE_CACHE_H
-
-#include"page.h"
-#include"file.h"
-
-#endif
